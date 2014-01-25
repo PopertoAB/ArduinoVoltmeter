f0ed88b03bd2c6fbd9e2cb7b15282853e9462da2
Manuel Cuevas <imscuevas@gmail.com>
2014-01-24 23:18:21 -0600

    Last modification
    With a lot of new things

--- ArduinoVoltmeter/ArduinoVoltmeter.ino
+++ ArduinoVoltmeter/ArduinoVoltmeter.ino
@@ -1,106 +1,81 @@
-#include <D7S.h>
+int entrada=2;
+int salida=13;
 
-int tiempoVista = 4;
+int lastButtonState=HIGH;
+int buttonState;
+long lastDebounceTime=0;
+long debounceDelay=50;
+long inicioPresion=0;
+long finPresion=0;
+int calibrar=0;
 int canal=0;
-int lectura;
-float lecturaFlotante;
-
-D7S dig1(5, tiempoVista);
-D7S dig2(4, tiempoVista);
-D7S dig3(1, tiempoVista);
-D7S dig4(0, tiempoVista);
+float calibracion=1.0;
 
 void setup(){
-  pinMode(2, INPUT);
+  Serial.begin(9600);
+  while(!Serial);
+
+  pinMode(entrada, INPUT_PULLUP);
+  pinMode(salida, OUTPUT);
+
+  inicioPresion=millis();
 }
 
 void loop(){
-  manejarInterrupcion();
-  checarCanal();
-  mostrarDatos();
+  leerBoton();
+  leerVoltaje();
 }
 
-void checarCanal(){
-  lectura=analogRead(canal);
-  lecturaFlotante=(float)(((float)lectura/1023.0)*20.0);
-  switch(canal){
-  case 0:
-    dig4.display(0x77);
-    break;
-  case 1:
-    dig4.display(0x7c);
-    break;
-  case 2:
-    dig4.display(0x39);
-    break;
-  case 3:
-    dig4.display(0x5e);
-    break;
-  case 4:
-    dig4.display(0x79);
-    break;
-  case 5:
-    dig4.display(0x71);
-    break;
+void leerBoton(){
+  int reading = digitalRead(entrada);
+
+  if(reading!=lastButtonState){
+    lastDebounceTime=millis();
   }
-}
 
-void mostrarDatos(){
-  dig1.display(ponerNumero(obtenerDecenas()));
-  dig2.display(ponerNumero(obtenerUnidades())|0x80);
-  dig3.display(ponerNumero(obtenerDecimales()));
-}
+  if((millis()-lastDebounceTime)>debounceDelay){
+    if(reading!=buttonState){
+      buttonState=reading;
 
-byte ponerNumero(int numero){
-  switch(numero){
-  case 0:
-    return 0x3f;
-  case 1:
-    return 0x06;
-  case 2:
-    return 0x5b;
-  case 3:
-    return 0x4f;
-  case 4:
-    return 0x66;
-  case 5:
-    return 0x6d;
-  case 6:
-    return 0x7d;
-  case 7:
-    return 0x07;
-  case 8:
-    return 0x7f;
-  case 9:
-    return 0x67;
-  default:
-    return 0x79;
-  }
-}
-
-int obtenerDecenas(){
-  int temp = ((int)lecturaFlotante/10)%10;
-  return temp;
-}
-
-int obtenerUnidades(){
-  int temp = (int)lecturaFlotante%10;
-  return temp;
-}
-
-int obtenerDecimales(){
-  int temp = (int)(lecturaFlotante*10.0)%10;
-  return temp;
-}
-
-void manejarInterrupcion(){
-  if(digitalRead(2)==0){
-    delay(50);
-    canal++;
-    if(canal>5){
-      canal=0;
+      if(buttonState==HIGH){
+        //Serial.println("ALTO");
+        finPresion=millis();
+        float presion=(float)(finPresion-inicioPresion)/1000.0;
+        if(presion>3.0){
+          calibrar=!calibrar;
+          if(calibrar==0){
+            Serial.println("No calibrando");
+          }
+          else if(calibrar==1){
+            Serial.println("Calibrando");
+          }
+        }
+        else{
+          Serial.print("Canal ");
+          Serial.println(canal);
+          if(canal++>=5){
+            canal=0;
+          }
+        }
+      }
+      else if(buttonState==LOW){
+        //Serial.println("BAJO"); 
+        inicioPresion=millis();     
+      }
     }
   }
+
+  lastButtonState=reading;
 }
 
-
+void leerVoltaje(){
+  if(!calibrar){
+    int voltaje=analogRead(canal);
+    long voltajeMap=map(voltaje,0,1023,0,200);
+    Serial.println(voltajeMap*calibracion);
+  }
+  else{
+    calibracion=(1023.0*5.0)/((float)analogRead(canal)*200.0);
+    Serial.println(calibracion);
+  }
+}
